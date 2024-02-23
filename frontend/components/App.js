4fd3af961db1bc23a1b0dfa3e1f8f77acc18128d
Marijana Taseska <marijana.taseska@gmail.com>
2024-02-23 11:47:09 -0800

    catch error message

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import axios from 'axios'
 
+
 const URL = 'http://localhost:9000/api/todos'
 
 
 export default class App extends React.Component {
   state={
     todos:[],
+    error:'',
   }
   fetchAllTodos = () =>{
 axios.get(URL)
@@ -14,7 +16,7 @@ axios.get(URL)
   this.setState({ ...this.state, todos: res.data.data})
 })
 .catch(err => {
-  debugger
+  this.setState({...this.state, error: err.response.data.message})
 })
   }
   componentDidMount(){
@@ -23,7 +25,7 @@ axios.get(URL)
   render() {
     return(
       <div>
-        <div id='error'>Error: No Error here</div>
+        <div id='error'>Error:{this.state.error}</div>
         <div id='todos'>
           <h2>Todos</h2>
           {
